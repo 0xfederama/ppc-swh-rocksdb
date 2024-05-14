3623a18b12e67ce155b2a353c4daaee80038748c
Federico Ramacciotti <vgfede@gmail.com>
2024-05-14 20:49:55 +0000

    new graphs, log scale

--- scripts/parquet_stats.py
+++ scripts/parquet_stats.py
@@ -1,54 +1,76 @@
 #!/usr/bin/env python3
 
-from operator import itemgetter
+import os
+import time
 from pyarrow.parquet import ParquetFile
 import matplotlib.pyplot as plt
 
 parquet_path = "/disk2/data/the-stack/the-stack-dedup_v1.parquet"
+# parquet_path = "/disk2/federico/the-stack/the-stack-small_256M.parquet"
 KiB = 1024
 MiB = 1024 * 1024
 GiB = 1024 * 1024 * 1024
-outliers_threshold = 256 * KiB
+outliers_threshold = 32 * KiB
+
+
+def build_graph(res_dict, xlabel, ylabel, name):
+    plt.figure(figsize=(16, 9))
+    plt.axvline(x=4, color="grey", linestyle="--")
+    # plt.axvline(x=256, color="grey", linestyle="--")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if name.startswith("log"):
+        plt.yscale("log")
+    plt.bar(list(res_dict.keys()), res_dict.values())
+    plt.savefig(f"graphs/{name}.png", format="png", dpi=120)
+    plt.close()
 
 
 if __name__ == "__main__":
-    pf = ParquetFile(parquet_path)
+    print(f"Starting at {time.asctime()}, pid: {os.getpid()}")
 
-    languages = {}
+    pf = ParquetFile(parquet_path)
+    languages_sizes = {}
+    languages_files = {}
     tot_size = 0
     max_size = 0
     n_files = 0
     outliers = 0
+    files_count = {}
     sizes_count = {}
+    sum_files_count = {}
+    sum_sizes_count = {}
     for batch in pf.iter_batches(columns=["lang", "size"]):
         for i, lang in enumerate(batch["lang"]):
             lang = str(lang)
             cont_size = int(str(batch["size"][i]))
-            languages[lang] = languages.get(lang, 0) + cont_size
+            languages_sizes[lang] = languages_sizes.get(lang, 0) + cont_size
+            languages_files[lang] = languages_files.get(lang, 0) + 1
             max_size = max(max_size, cont_size)
             n_files += 1
             tot_size += cont_size
-            if cont_size > outliers_threshold:
+            # size_kb = round(cont_size / KiB, 5)
+            size_kb = cont_size / KiB
+            if cont_size <= outliers_threshold:
+                files_count[size_kb] = files_count.get(size_kb, 0) + 1
+                sizes_count[size_kb] = sizes_count.get(size_kb, 0) + size_kb
+                sum_files = 0
+                sum_sizes = 0
+                for size, num in sum_sizes_count.items():
+                    if size < size_kb:
+                        sum_files += files_count[size]
+                        sum_sizes += sizes_count[size]
+                    if size > size_kb:
+                        sum_files_count[size] += 1
+                        sum_sizes_count[size] += size_kb
+                if size_kb in sum_files_count:
+                    sum_files_count[size_kb] += 1
+                    sum_sizes_count[size_kb] += size_kb
+                else:
+                    sum_files_count[size_kb] = sum_files + 1
+                    sum_sizes_count[size_kb] = sum_sizes + size_kb
+            else:
                 outliers += 1
-                continue
-            size_kb = round(cont_size / KiB, 1)
-
-            # approximate the result
-            # def custom_round(num, base):
-            #     return round(num / base) * base
-            # if cont_size > (8 * KiB):
-            #     size_kb = round(cont_size / KiB)
-            # elif cont_size > (4 * KiB):
-            #     size_kb = round(custom_round(cont_size / KiB, 0.5), 1)
-            #     # print(f"DBG 2 < s < 4: {size_kb}")
-            # else:
-            #     size_kb = round(custom_round(cont_size / KiB, 0.2), 1)
-            #     # print(f"DBG s < 2: {size_kb}")
-            sizes_count[size_kb] = sizes_count.get(size_kb, 0) + 1
-
-    sorted_lang = {
-        k: v for k, v in sorted(languages.items(), key=itemgetter(1), reverse=True)
-    }
 
     print(f"Total size: {round(tot_size / GiB, 3)} GiB")
     print(f"Max file size: {round(max_size / MiB, 3)} MiB")
@@ -58,17 +80,61 @@ if __name__ == "__main__":
         f"Outliers > {round(outliers_threshold / KiB)} KiB: {outliers} ({round(outliers * 100 / n_files, 3)} %)"
     )
 
-    plt.figure(figsize=(16, 9))
-    plt.axvline(x=4, color="grey", linestyle="--")
-    plt.axvline(x=256, color="grey", linestyle="--")
-    plt.xlabel("Size in KiB")
-    plt.ylabel("Number of files of that size")
-    plt.bar(list(sizes_count.keys()), sizes_count.values())
-    plt.savefig("fig.png", format="png", dpi=120)
+    # graph for files_count
+    build_graph(files_count, "Size in KiB", "Number of files = size", "files_count")
+
+    # graph for log_files_count
+    build_graph(
+        files_count, "Size in KiB", "Log of number of files = size", "log_files_count"
+    )
+
+    # graph for files_count
+    build_graph(
+        sum_files_count, "Size in KiB", "Number of files <= size", "sum_files_count"
+    )
+
+    # graph for log_files_count
+    build_graph(
+        sum_files_count,
+        "Size in KiB",
+        "Log of number of files <= size",
+        "log_sum_files_count",
+    )
+
+    # graph for sizes_count
+    build_graph(sizes_count, "Size in KiB", "Tot file size = size", "sizes_count")
+
+    # graph for log_sizes_count
+    build_graph(
+        sizes_count, "Size in KiB", "Log of tot file size = size", "log_sizes_count"
+    )
+
+    # graph for sizes_count
+    build_graph(
+        sum_sizes_count, "Size in KiB", "Tot file size <= size", "sum_sizes_count"
+    )
+
+    # graph for log_sizes_count
+    build_graph(
+        sum_sizes_count,
+        "Size in KiB",
+        "Log of tot file size <= size",
+        "log_sum_sizes_count",
+    )
 
     # print top x languages
-    # x = 10
-    # for idx, k in enumerate(sorted_lang):
-    #     if idx == x:
-    #         break
-    #     print(f"{k}: {str(round(sorted_lang[k] / MiB, 3))} MiB")
+    from operator import itemgetter
+
+    sorted_lang_files = {
+        k: v
+        for k, v in sorted(languages_files.items(), key=itemgetter(1), reverse=True)
+    }
+    i = 0
+    for lang, size in sorted_lang_files.items():
+        if i == 20:
+            break
+        print(
+            f"{lang}: {str(round(languages_sizes[lang] / GiB, 3))} GiB, {str(languages_files[lang])} files"
+        )
+
+    print(f"Ending at {time.asctime()}")
