45ebf719254728b014f9c5b62083bd367e8d4602
Federico Ramacciotti <vgfede@gmail.com>
2024-06-03 22:39:58 +0000

    new charts

--- scripts/parquet_stats.py
+++ scripts/parquet_stats.py
@@ -5,228 +5,275 @@ import os
 import time
 
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
+import matplotlib.patheffects as pe
 from pyarrow.parquet import ParquetFile
 
-# parquet_path = "/disk2/federico/the-stack/the-stack-small_10G.parquet"
 parquet_path = "/disk2/data/the-stack/the-stack-dedup_v1.parquet"
 charts_dir = "charts"
+info = ""
 KiB = 1024
 MiB = 1024 * 1024
 GiB = 1024 * 1024 * 1024
 
 
-def build_chart(x: list, y: list, xlab: str, ylab: str, name: str, logscale=False):
+def build_chart(x: list, y: list, xlab: str, ylab: str, name: str, loglog=False):
     plt.figure(figsize=(16, 9))
     # plot vertical lines
-    vlines = [4, 256]
-    for vline in vlines:
-        if max(x) >= vline:
-            plt.axvline(x=vline, color="grey", linestyle="--")
-            try:
-                index = x.index(vline)
-                inters_y = y[index]
-                if "sizes" in name:
-                    inters_y = round(inters_y / MiB, 3)
-                plt.text(
-                    vline,
-                    inters_y,
-                    f"({int(vline)}, {inters_y:.3f})",
-                    ha="center",
-                    va="bottom",
-                )
-                print(f"Vertical line at {vline} meets plot at {inters_y}")
-            except ValueError:
-                print(f"Value error for vline {vline}")
-    plt.xlabel(xlab)
-    plt.ylabel(ylab)
-    # set scale for y
-    if logscale:
-        ax = plt.gca()
+    if not loglog:
+        vlines = [4, 256]
+        for vline in vlines:
+            if max(x) >= vline:
+                plt.axvline(x=vline, color="grey", linestyle="--")
+                try:
+                    index = x.index(vline)
+                    inters_y = y[index]
+                    inters_y_str = inters_y
+                    if "sizes" in name:
+                        if "sum" in name:
+                            inters_y_str = str(round(inters_y * GiB / GiB, 2)) + " GiB"
+                        else:
+                            inters_y_str = str(round(inters_y * MiB / MiB, 2)) + " MiB"
+                    if "files" in name and "sum" in name:
+                        inters_y_str = str(round(inters_y / (10**8), 1)) + " x 10^8"
+                    plt.text(
+                        vline,
+                        inters_y,
+                        f"({int(vline)} KiB, {inters_y_str})",
+                        ha="center",
+                        va="bottom",
+                        color="white",
+                        fontsize="11",
+                        weight="bold",
+                        path_effects=[pe.withStroke(linewidth=4, foreground="red")],
+                    )
+                    print(f"Vertical line at {vline} meets plot at {inters_y}")
+                except ValueError:
+                    print(f"Value error for vline {vline}")
+    # manage logscale
+    if loglog:
         plt.yscale("log")
-        ax.yaxis.set_major_formatter(
-            mtick.LogFormatterExponent(base=10.0, labelOnlyBase=True)
-        )
+        plt.xscale("log")
+        y = sorted(y, reverse=True)
+        # import matplotlib.ticker as ticker
+        # ax = plt.gca()
+        # ax.yaxis.set_major_formatter(
+        #     ticker.LogFormatterExponent(base=10.0, labelOnlyBase=True)
+        # )
     else:
-        scilimits = (0, 0)
-        if "sizes" in name and "sum" in name:
-            scilimits = (6, 6)
-        plt.ticklabel_format(axis="y", style="sci", scilimits=scilimits)
+        plt.xlabel(xlab)
+        if name == "files":
+            scilimits = (3, 3)
+            plt.ticklabel_format(axis="y", style="sci", scilimits=scilimits)
+        else:
+            plt.ticklabel_format(style="plain", useOffset=False, axis="y")
     # draw the plot
+    plt.ylabel(ylab)
     plt.step(x, y, where="post")
     plt.fill_between(x, y, step="post")
     plt.ylim(bottom=0)
-    plt.savefig(f"{charts_dir}/{name}.png", format="png")
+    plt.savefig(f"{charts_dir}/{name}.png", format="png", bbox_inches="tight", dpi=120)
     plt.close()
-    print(f"Graph {name} created")
+    print(f"Graph {name} created\n")
 
 
 if __name__ == "__main__":
     print(f"Starting at {time.asctime()}, pid: {os.getpid()}")
     print(f"Parquet: {parquet_path}\n")
 
-    start = time.time()
     pf = ParquetFile(parquet_path)
-    lang_sizes = {}
+    lang_sizes_tot = {}
     lang_files = {}
+    lang_all_sizes = {}
+    lang_maxsizes = {}
     tot_size = 0
     max_size = 0
+    max_size_lang = None
     n_files = 0
-    outliers_32 = 0
-    outliers_256 = 0
-    outliers_32_size = 0
-    outliers_256_size = 0
+    outliers = {  # pairs of num_files, size
+        32: [0, 0],
+        64: [0, 0],
+        128: [0, 0],
+        256: [0, 0],
+        512: [0, 0],
+        1024: [0, 0],
+    }
     files_count_32 = {}
-    files_count_256 = {}
-    sizes_count_256 = {}
-    sizes_count_tot = {}
+    files_count = {}
+    sizes_count = {}
     for batch in pf.iter_batches(columns=["lang", "size"]):
         for i, lang in enumerate(batch["lang"]):
             lang = str(lang)
             size = int(str(batch["size"][i]))
-            lang_sizes[lang] = lang_sizes.get(lang, 0) + size
+            lang_sizes_tot[lang] = lang_sizes_tot.get(lang, 0) + size
             lang_files[lang] = lang_files.get(lang, 0) + 1
-            max_size = max(max_size, size)
+            langlist = []
+            size_kb = round(size / KiB, 3)
+            if lang_all_sizes.get(lang) is None:
+                lang_all_sizes[lang] = []
+            lang_all_sizes[lang].append(size_kb)
+            if size > max_size:
+                max_size = size
+                max_size_lang = lang
+            if size > lang_maxsizes.get(lang, 0):
+                lang_maxsizes[lang] = size
             n_files += 1
             tot_size += size
-            size_kb = round(size / KiB, 3)
-            sizes_count_tot[size_kb] = sizes_count_tot.get(size_kb, 0) + size_kb
-            if size > 256 * KiB:
-                outliers_32 += 1
-                outliers_32_size += size
-                outliers_256 += 1
-                outliers_256_size += size
-            else:
-                files_count_256[size_kb] = files_count_256.get(size_kb, 0) + 1
-                sizes_count_256[size_kb] = sizes_count_256.get(size_kb, 0) + size_kb
-                if size > 32 * KiB:
-                    outliers_32 += 1
-                    outliers_32_size += size
-                else:
-                    files_count_32[size_kb] = files_count_32.get(size_kb, 0) + 1
-
-    end = time.time()
-    print(f"Read file: {round(end-start)} s")
-    start = time.time()
+            sizes = [32, 64, 128, 256, 512, 1024]
+            for s in sizes:
+                if size > s * KiB:
+                    outliers[s][0] += 1
+                    outliers[s][1] += size
+            if size <= 1 * MiB:
+                files_count[size] = files_count.get(size, 0) + 1
+                sizes_count[size] = sizes_count.get(size, 0) + size
+                if size <= 32 * KiB:
+                    files_count_32[size] = files_count_32.get(size, 0) + 1
 
     # sort the dictionaries
+    files_count = dict(sorted(files_count.items()))
+    sizes_count = dict(sorted(sizes_count.items()))
     files_count_32 = dict(sorted(files_count_32.items()))
-    files_count_256 = dict(sorted(files_count_256.items()))
-    sizes_count_256 = dict(sorted(sizes_count_256.items()))
-    sizes_count_tot = dict(sorted(sizes_count_tot.items()))
 
     # get the axis lists
+    x_axis = list(files_count.keys())
+    y_files = list(files_count.values())
+    y_sizes = list(sizes_count.values())
     x_axis_32 = list(files_count_32.keys())
-    x_axis_256 = list(files_count_256.keys())
-    x_axis_tot = list(sizes_count_tot.keys())
-
     y_files_32 = list(files_count_32.values())
-    y_files_256 = list(files_count_256.values())
-    y_sizes_256 = list(sizes_count_256.values())
-    y_sizes_tot = list(sizes_count_tot.values())
+
+    x_axis_kb_32 = list(map(lambda n: round(n / KiB, 3), x_axis_32))
+    x_axis_kb = list(map(lambda n: round(n / KiB, 3), x_axis))
+    y_sizes_mb = list(map(lambda n: round(n / MiB, 6), y_sizes))
 
     # get the axis with the cumulative values
-    y_files_256_sum = list(itertools.accumulate(y_files_256))
-    y_sizes_256_sum = list(itertools.accumulate(y_sizes_256))
-    y_sizes_tot_sum = list(itertools.accumulate(y_sizes_tot))
-
-    end = time.time()
-    print(f"Sort and accumulate: {round(end-start)} s\n")
+    y_files_sum = list(itertools.accumulate(y_files))
+    y_sizes_sum = list(itertools.accumulate(y_sizes))
+    y_sizes_sum_gb = list(map(lambda n: round(n / GiB, 9), y_sizes_sum))
 
     print(f"Total size: {round(tot_size / GiB, 3)} GiB")
-    print(f"Max file size: {round(max_size / MiB, 3)} MiB")
+    print(f"Max file size: {round(max_size / MiB, 3)} MiB, language {max_size_lang}")
     print(f"Average file size: {round((tot_size / KiB) / n_files, 3)} KiB")
     print(f"Number of files: {n_files}")
     print(
-        f"Outliers > 32 KiB: {outliers_32} ({round(outliers_32 * 100 / n_files, 3)} %), {round(outliers_32_size / MiB, 3)} MiB ({round(outliers_32_size * 100 / tot_size, 3)} %)"
+        f"Outliers > 32 KiB: {outliers[32][0]} ({round(outliers[32][0] * 100 / n_files, 3)} %), {round(outliers[32][1] / MiB, 3)} MiB ({round(outliers[32][1] * 100 / tot_size, 3)} %)"
     )
     print(
-        f"Outliers > 256 KiB: {outliers_256} ({round(outliers_256 * 100 / n_files, 3)} %), {round(outliers_256_size / MiB, 3)} MiB ({round(outliers_256_size * 100 / tot_size, 3)} %)"
+        f"Outliers > 64 KiB: {outliers[64][0]} ({round(outliers[64][0] * 100 / n_files, 3)} %), {round(outliers[64][1] / MiB, 3)} MiB ({round(outliers[64][1] * 100 / tot_size, 3)} %)"
+    )
+    print(
+        f"Outliers > 128 KiB: {outliers[128][0]} ({round(outliers[128][0] * 100 / n_files, 3)} %), {round(outliers[128][1] / MiB, 3)} MiB ({round(outliers[128][1] * 100 / tot_size, 3)} %)"
+    )
+    print(
+        f"Outliers > 256 KiB: {outliers[256][0]} ({round(outliers[256][0] * 100 / n_files, 3)} %), {round(outliers[256][1] / MiB, 3)} MiB ({round(outliers[256][1] * 100 / tot_size, 3)} %)"
+    )
+    print(
+        f"Outliers > 512 KiB: {outliers[512][0]} ({round(outliers[512][0] * 100 / n_files, 3)} %), {round(outliers[512][1] / MiB, 3)} MiB ({round(outliers[512][1] * 100 / tot_size, 3)} %)"
+    )
+    print(
+        f"Outliers > 1 MiB: {outliers[1024][0]} ({round(outliers[1024][0] * 100 / n_files, 5)} %), {round(outliers[1024][1] / MiB, 3)} MiB ({round(outliers[1024][1] * 100 / tot_size, 3)} %)"
     )
     print()
 
     os.mkdir(charts_dir)
     with open(f"{charts_dir}/info.txt", "w") as f:
-        info = ""
         f.write(f"{parquet_path}\n{info}")
 
-    # graph for files_count
+    # build files to 32K
     build_chart(
-        x_axis_32,
+        x_axis_kb_32,
         y_files_32,
         "Size in KiB",
-        "Number of files whose size is equal to x",
+        "Number of files having a given size",
         "files",
     )
 
-    # graph for log_files_count
+    # build files loglog 1M
+    build_chart(
+        x_axis,
+        y_files,
+        "Log size in KiB",
+        "Log number of files having a given size",
+        "files_log_log_1M",
+        loglog=True,
+    )
+
+    # build cumulative files to 1M
     # build_chart(
-    #     x_axis_256,
-    #     y_files_256,
+    #     x_axis_kb,
+    #     y_files_sum,
     #     "Size in KiB",
-    #     "Log of number of files = size",
-    #     "files_count_log",
-    #     logscale=True,
+    #     "Number of files having at most a given size",
+    #     "files_sum",
     # )
 
-    # graph for sum_files_count
+    # build sizes to 1M
     build_chart(
-        x_axis_256,
-        y_files_256_sum,
+        x_axis_kb,
+        y_sizes_mb,
         "Size in KiB",
-        "Number of files whose size is less than or equal to x",
-        "files_sum",
-    )
-
-    # graph for sizes_count
-    build_chart(
-        x_axis_256,
-        y_sizes_256,
-        "Size in KiB",
-        "Total file size of files whose size is equal to x",
+        "Space in MiB of files having a given size",
         "sizes",
     )
-    build_chart(
-        x_axis_tot,
-        y_sizes_tot,
-        "Size in KiB",
-        "Total file size of files whose size is equal to x",
-        "sizes_tot",
-    )
 
-    # graph for sum_sizes_count
+    # build cumulative sizes to 1M
     build_chart(
-        x_axis_256,
-        y_sizes_256_sum,
+        x_axis_kb,
+        y_sizes_sum_gb,
         "Size in KiB",
-        "Total file size of files whose size is less than or equal to x",
+        "Space in GiB of files having at most a given size",
         "sizes_sum",
     )
-    build_chart(
-        x_axis_tot,
-        y_sizes_tot_sum,
-        "Size in KiB",
-        "Total file size of files whose size is less than or equal to x",
-        "sizes_tot_sum",
-    )
-
-    print(f"\nFinished graphs, {time.asctime()}")
 
     # print top x languages
     from operator import itemgetter
 
     sorted_lang_sizes = {
-        k: v for k, v in sorted(lang_sizes.items(), key=itemgetter(1), reverse=True)
+        k: v for k, v in sorted(lang_sizes_tot.items(), key=itemgetter(1), reverse=True)
     }
     i = 0
     top = 20
-    print(f"\nTop {top} languages:")
-    for lang, size in sorted_lang_sizes.items():
+    smalllang = {}
+    biglang = {}
+    print(f"Top {top} languages:")
+    for lang, _ in sorted_lang_sizes.items():
         if i == top:
             break
+        nfiles = lang_files[lang]
+        langsize_gb = round(lang_sizes_tot[lang] / GiB, 3)
+        avg_kb = round(lang_sizes_tot[lang] / lang_files[lang] / KiB, 3)
+        max_kb = round(lang_maxsizes[lang] / MiB, 3)
         print(
-            f"{lang}: {str(lang_files[lang])} files, {str(round(lang_sizes[lang] / GiB, 3))} GiB, {str(round(lang_sizes[lang] / lang_files[lang] / KiB, 3))} KiB avg size"
+            f"{lang}: {nfiles} files, {langsize_gb} GiB, {avg_kb} KiB avg size, {max_kb} MiB max size"
         )
         i += 1
+        biglangs = ["Jupyter Notebook", "CSV"]  # , "JSON"]
+        if lang not in biglangs:
+            smalllang[lang] = lang_all_sizes[lang]
+        else:
+            biglang[lang] = lang_all_sizes[lang]
+
+    name = "lang_boxplot_small"
+    x = list(smalllang.keys())
+    y = list(smalllang.values())
+    plt.figure(figsize=(16, 9))
+    plt.ylabel("File sizes in KiB")
+    plt.xlabel("Languages")
+    plt.xticks(rotation=45, ha="right")
+    plt.boxplot(y, labels=x, patch_artist=True, showfliers=False)
+    plt.savefig(f"{charts_dir}/{name}.png", format="png", bbox_inches="tight", dpi=120)
+    plt.close()
+    print(f"\nGraph {name} created")
+
+    name = "lang_boxplot_big"
+    x = list(biglang.keys())
+    y = list(biglang.values())
+    plt.figure(figsize=(16, 9))
+    plt.ylabel("File sizes in KiB")
+    plt.xlabel("Languages")
+    plt.xticks(rotation=45, ha="right")
+    plt.boxplot(y, labels=x, patch_artist=True, showfliers=False)
+    plt.savefig(f"{charts_dir}/{name}.png", format="png", bbox_inches="tight", dpi=120)
+    plt.close()
+    print(f"\nGraph {name} created")
 
     print(f"\nEnding at {time.asctime()}")
