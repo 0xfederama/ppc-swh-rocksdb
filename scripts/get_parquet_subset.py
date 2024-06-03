f7985cc77c0ef7094ca00933e6dc253222e325f6
Federico Ramacciotti <vgfede@gmail.com>
2024-06-03 22:44:33 +0000

    new parquet subset script

--- scripts/get_parquet_subset.py
+++ scripts/get_parquet_subset.py
@@ -9,18 +9,17 @@ KiB = 1024
 MiB = 1024 * 1024
 GiB = 1024 * 1024 * 1024
 
-size = 4 * GiB  # float('inf') to take all the files
-size_str = "4G"
-minsize = 990 * KiB  # 0 to take files with all sizes
-minsize_str = "990K"
+size = float("inf")  # float('inf') to take all the files
+size_str = "inf"
+minsize = 1 * MiB  # 0 to take files with all sizes
+minsize_str = "1M"
 
 parq_path = "/disk2/data/the-stack/the-stack-dedup_v1.parquet"
-
 
 if __name__ == "__main__":
     print(f"Starting at {time.asctime()}, pid: {os.getpid()}")
     print(
-        f"Getting a subset of {size_str} from {parq_path} with min size {minsize_str}"
+        f"Getting a subset of {size_str if size != float('inf') else 'inf'} from {parq_path} with min size {minsize_str}"
     )
     start_time = time.time()
 
@@ -40,17 +39,20 @@ if __name__ == "__main__":
                     break
         if tot_size >= size:
             break
+
     df = pd.DataFrame(dataframes)
     print(f"Total size reached: {round(tot_size / GiB, 3)} GiB")
     print(f"Total number of files: {tot_files}")
-    if minsize > 0:
+    if size == float("inf"):
+        size_str = round(tot_size / GiB) + "G"
+    if minsize == 0:
         filename = f"/disk2/federico/the-stack/the-stack-{size_str}.parquet"
     else:
         filename = f"/disk2/federico/the-stack/the-stack-{size_str}_minsize_{minsize_str}.parquet"
-    df.to_parquet(filename)
+    df.to_parquet(filename, compression=None)
 
     end_time = time.time()
     tot_time = end_time - start_time
 
-    print(f"Parquet created in {round(tot_time)} s")
+    print(f"Parquet {filename} created in {round(tot_time)} s")
     print(f"Ending at {time.asctime()}")
