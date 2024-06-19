e6e0f859bf1348f5214234659353f49a03b29ac2
Federico Ramacciotti <vgfede@gmail.com>
2024-06-19 16:13:01 +0000

    add prints to script

--- scripts/create_dir_from_parquet.py
+++ scripts/create_dir_from_parquet.py
@@ -7,6 +7,8 @@ parq_size = "10G"  # 5rec, 1M, 8M, 64M, 256M, 1G, 4G, 10G, 200G, dedup_v1, 1G_mi
 small_parq_path = "/disk2/federico/the-stack/small/the-stack-" + parq_size + ".parquet"
 full_parq_path = "/disk2/federico/the-stack/the-stack-" + parq_size + ".parquet"
 parq_path = small_parq_path if "dedup_v1" not in parq_size else full_parq_path
+parq_size = "60G-py"
+parq_path = "/disk2/federico/the-stack/langs/the-stack-" + parq_size + ".parquet"
 blobs_path = f"/disk2/federico/blobs/{parq_size}"
 
 KiB = 1024
@@ -15,6 +17,7 @@ GiB = 1024 * 1024 * 1024
 
 if __name__ == "__main__":
     print(f"Starting at {time.asctime()}, pid: {os.getpid()}")
+    print(f"Creating a directory from parquet {parq_path} in {blobs_path}")
 
     # create blobs directory. if it exists and is not empty, crash
     try:
@@ -27,6 +30,7 @@ if __name__ == "__main__":
     pf = ParquetFile(parq_path)
     start = time.time()
     rows = []  # list of dictionary rows
+    tot_size = 0
     for batch in pf.iter_batches(
         columns=[
             "hexsha",
@@ -42,6 +46,7 @@ if __name__ == "__main__":
             filename = str(batch["max_stars_repo_path"][i])
             repo = str(batch["max_stars_repo_name"][i])
             size = int(str(batch["size"][i]))
+            tot_size += size
             local_path = f"{sha[0:2]}/{sha[2:4]}"
             # create the csv row
             new_row = {
@@ -50,20 +55,20 @@ if __name__ == "__main__":
                 "length": size,
                 "filename": filename,
                 "filepath": os.path.join(repo, filename),
-                "local_path": local_path,  # FIXME: this should be the path to the blob with the directories of the hash
+                "local_path": local_path,
             }
             rows.append(new_row)
 
             # create the file in the directory
-            try:
-                os.makedirs(os.path.join(blobs_path, local_path))
-            except FileExistsError:
-                # if the dir already exists, simply create the file in it
-                pass
+            os.makedirs(os.path.join(blobs_path, local_path), exist_ok=True)
             with open(os.path.join(blobs_path, local_path, sha), "w") as f:
                 f.write(content)
 
+    print(f"Created the directory with total size {round(tot_size / GiB, 2)} GiB")
+
     df = pd.DataFrame(rows)
-    df.to_csv(f"/disk2/federico/blobs/{parq_size}_list_of_files.csv", index=False)
+    filelist_path = f"/disk2/federico/blobs/{parq_size}_list_of_files.csv"
+    df.to_csv(filelist_path, index=False)
+    print(f"Files list created in {filelist_path}")
 
     print(f"Ending at {time.asctime()}")
