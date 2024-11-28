034ec4646f2c3685f6e37bcd4721366f37116813
ftosoni <francesco.tosoni@phd.unipi.it>
2024-11-28 15:40:17 +0000

    improve example's syntax.

--- example/example.py
+++ example/example.py
@@ -3,18 +3,28 @@ import time
 
 import pyarrow.parquet as pq
 
-import lib
+import aimrocks
+
+import lib, os
 
 KiB = 1024
 MiB = 1024 * 1024 * 1024
 
 
-if __name__ == "__main__":
-    db = lib.DB_PPC("/path/to/test_dir/")
-    db.create_db()
+def main() :
+    test_dir = 'test'
+    parquet_path = '../data/the-stack-64M.parquet'
+
+    os.makedirs(test_dir, exist_ok=True)
+
+    db = lib.DB_PPC(test_dir)
+    try:
+        db.create_db()
+    except Exception as e:  
+        print(f'Error creating DB: {e}')
+        return
 
     # read the parquet and insert into the db
-    parquet_path = "/path/to/file.parquet"
     parquet_file = pq.ParquetFile(parquet_path)
     max_size = 1 * MiB
     keys_to_get = []
@@ -24,29 +34,29 @@ if __name__ == "__main__":
     tot_size = 0
     for batch in parquet_file.iter_batches(
         columns=[
-            "hexsha",
-            "max_stars_repo_path",
-            "content",
-            "size",
-            "lang",
+            'hexsha',
+            'max_stars_repo_path',
+            'content',
+            'size',
+            'lang',
         ]
     ):
         batch_list = batch.to_pylist()
         for row in batch_list:
             key = db.make_key(
-                row["hexsha"], row["max_stars_repo_path"], int(row["size"]), max_size
+                row['hexsha'], row['max_stars_repo_path'], int(row['size']), max_size
             )
             start = time.time()
-            db.insert_single(key.encode(), row["content"].encode())
+            db.insert_single(key.encode(), row['content'].encode())
             end = time.time()
             ins_time += end - start
-            tot_size += int(row["size"])
+            tot_size += int(row['size'])
             # save 10% of keys for retrieval
             if random.random() < 0.1:
                 keys_to_get.append(key.encode())
-    print(f"Insertion throughput: {round(tot_size / MiB / ins_time, 2)} MiB/s")
+    print(f'Insertion throughput: {round(tot_size / MiB / ins_time, 2)} MiB/s')
 
-    print("Getting", len(keys_to_get))
+    print('Getting', len(keys_to_get))
 
     # Test single-get
     sg_time = 0
@@ -57,11 +67,14 @@ if __name__ == "__main__":
         end = time.time()
         sg_time += end - start
         got_size += len(value)
-    print(f"Single-get throughput: {round(got_size / MiB / sg_time, 2)} MiB/s")
+    print(f'Single-get throughput: {round(got_size / MiB / sg_time, 2)} MiB/s')
 
     # Test multi-get
     start = time.time()
     values = db.multi_get(keys_to_get)
     end = time.time()
     mg_time = end - start
-    print(f"Single-get throughput: {round(got_size / MiB / mg_time, 2)} MiB/s")
+    print(f'Multi-get throughput: {round(got_size / MiB / mg_time, 2)} MiB/s')
+
+if __name__ == '__main__':
+    main()
